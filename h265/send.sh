dc6e625cb0c07fd7df2424ee731445cdf0969c64
lab_pele <>
2023-06-13 09:36:37 +0300

    send/receive video scripts update

--- h265/send.sh
+++ h265/send.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 IP="127.0.0.1"
-gst-launch-1.0 videotestsrc ! videoconvert ! queue ! x265enc tune=zerolatency ! video/x-h265, stream-format=byte-stream ! rtph265pay ! udpsink host=$IP port=5000
+PORT=5000
+WIDTH=640
+HEIGHT=480
+RATE=25
+gst-launch-1.0 videotestsrc ! video/x-raw,width=$WIDTH,height=$HEIGHT,framerate=$RATE/1 ! videoconvert ! queue ! x265enc tune=zerolatency ! video/x-h265, stream-format=byte-stream ! rtph265pay config-interval=1 ! udpsink host=$IP port=$PORT
 
--- send_video.sh
+++ send_video.sh
@@ -2,6 +2,7 @@
 
 # Default vals
 IP="127.0.0.1"
+PORT=5000
 H264=false
 
 # Parse args
@@ -23,9 +24,9 @@ done
 
 if $H264 ; then
     echo "Sending H264 stream to $IP"
-    gst-launch-1.0 videotestsrc ! videoconvert ! queue ! x264enc tune=zerolatency ! video/x-h264, stream-format=byte-stream ! rtph264pay ! udpsink host=$IP port=5000
+    gst-launch-1.0 videotestsrc ! videoconvert ! queue ! x264enc tune=zerolatency ! video/x-h264, stream-format=byte-stream ! rtph264pay config-interval=1 ! udpsink host=$IP port=$PORT
 else
     echo "Sending H265 stream to $IP"
-    gst-launch-1.0 videotestsrc ! videoconvert ! queue ! x265enc tune=zerolatency ! video/x-h265, stream-format=byte-stream ! rtph265pay ! udpsink host=$IP port=5000
+    gst-launch-1.0 videotestsrc ! videoconvert ! queue ! x265enc tune=zerolatency ! video/x-h265, stream-format=byte-stream ! rtph265pay config-interval=1 ! udpsink host=$IP port=$PORT
 fi
 
