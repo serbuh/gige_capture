39b7daf15f2f6e2fae85f3ad3bfa486a4b9c0c8a
lab_pele <>
2023-06-28 13:25:31 +0300

    fix binning

--- video_feeders/arv_cam.py
+++ video_feeders/arv_cam.py
@@ -88,12 +88,19 @@ class ArvCamera(VideoFeeder):
         try:
             if self.cam_config.binning >= 0:
                 self.arv_camera.set_integer("BinningVertical", int(self.cam_config.binning))
-            binning_x, binning_y = self.arv_camera.get_binning()
-            self.logger.info(f"Binning       : {binning_x}, {binning_y}")
+                binning_x, binning_y = self.arv_camera.get_binning()
+                self.logger.info(f"Binning       : {binning_x}, {binning_y}")
         except gi.repository.GLib.Error as e:
             self.logger.error(f"{e}\nCould not set binning")
             return False, None
-                
+        
+        # Remove pattern from pleora
+        try:
+            self.arv_camera.set_string("TestImageSelector", "Off")
+        except gi.repository.GLib.Error as e:
+            self.logger.error(f"{e}\nCould not remove pattern")
+            return False, None
+        
         # Set frame rate
         try:
             if self.cam_config.grab_fps >= 0:
